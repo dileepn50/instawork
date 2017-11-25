e224d0c20b6feb170cde5e5c68d358decbe9948f
dileepn <dileepn@akamai.com>
2017-11-25 23:06:05 +0530

    Som field contraints are added to the models.py

--- app/models.py
+++ app/models.py
@@ -14,8 +14,8 @@ class Team(models.Model):
     last_name = models.CharField(max_length=200)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)  # validators should be a list
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True, unique=True)  # validators should be a list
 
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     role = models.CharField(choices=role_choices, max_length=10)
 
